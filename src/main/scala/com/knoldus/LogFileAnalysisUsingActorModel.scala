20b518a6f90210800fcc49adb89e8279b7acc6b8
mansi7babbar <mansi7babbar@gmail.com>
2020-02-24 13:11:33 +0530

    updated akka assignment

--- src/main/scala/com/knoldus/LogFileAnalysisUsingActorModel.scala
+++ src/main/scala/com/knoldus/LogFileAnalysisUsingActorModel.scala
@@ -12,27 +12,47 @@ class LogAnalysis extends Actor with ActorLogging {
 
   override def receive: Receive = {
     case LogFiles(files) =>
-      log.info(files.map(file => (file, Source.fromFile(file).getLines.toList.count(line => line.contains("[ERROR]")),
-        Source.fromFile(file).getLines.toList.count(line => line.contains("[WARN]")),
-        Source.fromFile(file).getLines.toList.count(line => line.contains("[INFO]")))).toString)
+      log.info(LogAnalysis.getErrorsWarningsInfoPerFile(files).toString)
+      log.info(LogAnalysis.getAvgErrors(files).toString)
+      log.info(LogAnalysis.getAvgWarnings(files).toString)
+      log.info(LogAnalysis.getAvgInfo(files).toString)
   }
 }
 
 case class LogFiles(logFiles: List[File])
 
-object LogFileAnalysisUsingActorModel extends App {
-  implicit val timeout: Timeout = Timeout(20 second)
+object LogAnalysis {
+  def getErrorsWarningsInfoPerFile(logFiles: List[File]): List[(File, Int, Int, Int)] = {
+    logFiles.map(file => (file, Source.fromFile(file).getLines.toList.count(line => line.contains("[ERROR]")),
+      Source.fromFile(file).getLines.toList.count(line => line.contains("[WARN]")),
+      Source.fromFile(file).getLines.toList.count(line => line.contains("[INFO]"))))
+  }
 
+  def getAvgErrors(logFiles: List[File]): List[(File, Int)] = {
+    logFiles.map(file => (file, Source.fromFile(file).getLines.toList.count(line => line.contains("[ERROR]")) / Source.fromFile(file).getLines.toList.length))
+  }
+
+  def getAvgWarnings(logFiles: List[File]): List[(File, Int)] = {
+    logFiles.map(file => (file, Source.fromFile(file).getLines.toList.count(line => line.contains("[WARN]")) / Source.fromFile(file).getLines.toList.length))
+  }
+
+  def getAvgInfo(logFiles: List[File]): List[(File, Int)] = {
+    logFiles.map(file => (file, Source.fromFile(file).getLines.toList.count(line => line.contains("[INFO]")) / Source.fromFile(file).getLines.toList.length))
+  }
+}
+
+object ListOfFiles {
   def getListOfFiles(directoryName: String): List[File] = {
     val dir = new File(directoryName)
     val files = dir.listFiles.toList
     files
   }
+}
 
-  val logFiles = getListOfFiles("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources")
-
+object LogFileAnalysisUsingActorModel extends App {
+  implicit val timeout: Timeout = Timeout(20 second)
+  val logFiles = ListOfFiles.getListOfFiles("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources")
   val system = ActorSystem("LogFileAnalysisSystem")
   val logAnalysis = system.actorOf(Props[LogAnalysis])
   logAnalysis ! LogFiles(logFiles)
-
 }
