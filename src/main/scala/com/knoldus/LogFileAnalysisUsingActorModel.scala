88225e5e4e32de744d53cc5b126fe5153994c553
mansi7babbar <mansi7babbar@gmail.com>
2020-02-24 10:20:42 +0530

    updated akka assignment

--- src/main/scala/com/knoldus/LogFileAnalysisUsingActorModel.scala
+++ src/main/scala/com/knoldus/LogFileAnalysisUsingActorModel.scala
@@ -4,42 +4,22 @@ import java.io.File
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.util.Timeout
+import akka.pattern._
 
 import scala.concurrent.duration._
 import scala.io.Source
 
-class TotalNoOfErrors extends Actor with ActorLogging {
-  var errorCounter = 0
+class LogAnalysis extends Actor with ActorLogging {
 
   override def receive: Receive = {
-    case count: Int => errorCounter += count
+    case LogFiles(files) =>
+      log.info(files.map(file => (file, Source.fromFile(file).getLines.toList.count(line => line.contains("[ERROR]")),
+        Source.fromFile(file).getLines.toList.count(line => line.contains("[WARN]")),
+        Source.fromFile(file).getLines.toList.count(line => line.contains("[INFO]")))).toString)
   }
-
-  val avgErrorsPerFile: Int = errorCounter / LogFileAnalysisUsingActorModel.logFiles.length
-  log.info(s"Average errors per file are $avgErrorsPerFile")
 }
 
-class TotalNoOfWarnings extends Actor with ActorLogging {
-  var warningCounter = 0
-
-  override def receive: Receive = {
-    case count: Int => warningCounter += count
-  }
-
-  val avgWarningsPerFile: Int = warningCounter / LogFileAnalysisUsingActorModel.logFiles.length
-  log.info(s"Average warnings per file are $avgWarningsPerFile")
-}
-
-class TotalNoOfInfo extends Actor with ActorLogging {
-  var infoCounter = 0
-
-  override def receive: Receive = {
-    case count: Int => infoCounter += count
-  }
-
-  val avgInfoPerFile: Int = infoCounter / LogFileAnalysisUsingActorModel.logFiles.length
-  log.info(s"Average info per file are $avgInfoPerFile")
-}
+case class LogFiles(logFiles: List[File])
 
 object LogFileAnalysisUsingActorModel extends App {
   implicit val timeout: Timeout = Timeout(5 second)
@@ -53,14 +33,7 @@ object LogFileAnalysisUsingActorModel extends App {
   val logFiles = getListOfFiles("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources")
 
   val system = ActorSystem("LogFileAnalysisSystem")
-  val totalNoOfErrors = system.actorOf(Props[TotalNoOfErrors])
-  val totalNoOfWarnings = system.actorOf(Props[TotalNoOfWarnings])
-  val totalNoOfInfo = system.actorOf(Props[TotalNoOfInfo])
-
-  for (file <- logFiles) {
-    totalNoOfErrors ! Source.fromFile(file).getLines.toList.count(line => line.contains("[ERROR]"))
-    totalNoOfWarnings ! Source.fromFile(file).getLines.toList.count(line => line.contains("[WARN]"))
-    totalNoOfInfo ! Source.fromFile(file).getLines.toList.count(line => line.contains("[INFO]"))
-  }
+  val logAnalysis = system.actorOf(Props[LogAnalysis])
+  logAnalysis ? LogFiles(logFiles)
 
 }
