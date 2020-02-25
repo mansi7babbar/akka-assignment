78c054d5649b623d562d699e37afd085b9631c90
mansi7babbar <mansi7babbar@gmail.com>
2020-02-25 06:57:58 +0530

    updated akka assignment

--- src/main/scala/com/knoldus/LogFileAnalysisUsingDispatcher.scala
+++ src/main/scala/com/knoldus/LogFileAnalysisUsingDispatcher.scala
@@ -3,12 +3,14 @@ package com.knoldus
 import java.io.File
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.dispatch.MessageDispatcher
+import akka.routing.RoundRobinPool
 
 import scala.io.Source
 
-case class LogRecord(file: File, errorCount: Int, warnCount: Int, infoCount: Int)
+case class LogRecords(file: File, errorCount: Int, warnCount: Int, infoCount: Int)
 
-class LogsAnalysis extends Actor with ActorLogging {
+class LogsAnalytics extends Actor with ActorLogging {
   override def receive: Receive = {
     case file: File =>
       val res = Source.fromFile(file).getLines.toList.foldLeft(file, 0, 0, 0) { (count, elem) =>
@@ -25,25 +27,27 @@ class LogsAnalysis extends Actor with ActorLogging {
           (count._1, count._2, count._3, count._4)
         }
       }
-      log.info(LogRecord(res._1, res._2, res._3, res._4).toString)
+      log.info(LogRecords(res._1, res._2, res._3, res._4).toString)
   }
 }
 
-class Logs extends Actor with ActorLogging {
+class Log extends Actor with ActorLogging {
   override def receive: Receive = {
     case directoryName: String =>
       val dir = new File(directoryName)
       val logFiles = dir.listFiles.toList
       for (file <- logFiles) {
-        val logsAnalysis = context.actorOf(Props[LogsAnalysis])
+        val props = RoundRobinPool(5).props(Props[LogsAnalytics])
+        val logsAnalysis = context.actorOf(props.withDispatcher("fixed-thread-pool"))
         logsAnalysis ! file
       }
   }
 }
 
-object LogFileAnalysisUsingActorModel extends App {
+object LogFileAnalysisUsingDispatcher extends App {
   val directoryName = "/home/knoldus/IdeaProjects/akka-assignment/src/main/resources"
   val system = ActorSystem("LogFileAnalysisSystem")
-  val logFiles = system.actorOf(Props[Logs])
+  implicit val executionContext: MessageDispatcher = system.dispatchers.lookup("fixed-thread-pool")
+  val logFiles = system.actorOf(Props[Log])
   logFiles ! directoryName
 }
