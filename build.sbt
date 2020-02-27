b9c58de614a6d51e157b3c01ad38b631a957dd09
mansi7babbar <mansi7babbar@gmail.com>
2020-02-27 10:51:14 +0530

    updated akka assignment

--- build.sbt
+++ build.sbt
@@ -8,3 +8,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.12",
   "com.typesafe.akka" %% "akka-testkit" % "2.5.12" % Test
 )
+
+libraryDependencies += "com.typesafe.akka" %% "akka-testkit" % "2.6.3" % Test
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0" % Test
---
+++ src/test/scala/com/knoldus/logfile_analysis/LogFilesSpec.scala
@@ -0,0 +1,35 @@
+package com.knoldus.logfile_analysis
+
+import java.io.File
+
+import org.scalatest._
+
+class LogFilesSpec extends FlatSpec with BeforeAndAfterAll {
+
+  var logFilesAnalysis: LogFileAnalysis = new LogFileAnalysis
+  var logFilesAnalysisForAvg: LogFileAnalysisForAvg = new LogFileAnalysisForAvg
+
+  override def beforeAll(): Unit = {
+    logFilesAnalysis = new LogFileAnalysis
+    logFilesAnalysisForAvg = new LogFileAnalysisForAvg
+  }
+
+  override def afterAll(): Unit = {
+    if (logFilesAnalysis != null & logFilesAnalysisForAvg != null) {
+      logFilesAnalysis = null
+      logFilesAnalysisForAvg = null
+    }
+  }
+
+  "getLogRecord method" should "return list of objects of case class LogRecord" in {
+    val actualResult = logFilesAnalysis.getLogRecord
+    val expectedResult = List(LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log5"), 0, 1454, 256), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log9"), 87, 1259, 272), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log2"), 0, 1446, 245), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log7"), 0, 1446, 245), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log8"), 0, 1446, 245), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log11"), 89, 1263, 272), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log1"), 91, 1268, 278), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log10"), 91, 1268, 278), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log4"), 0, 1454, 256), LogRecord(new File("/home/knoldus/IdeaProjects/akka-assignment/src/main/resources/res/stdout-log12"), 91, 1268, 278))
+    assert(expectedResult == actualResult)
+  }
+
+  "getLogRecordAvg method" should "return object of case class LogRecordAvg" in {
+    val actualResult = logFilesAnalysisForAvg.getLogRecordAvg
+    val expectedResult = LogRecordAvg(44,1357,262)
+    assert(expectedResult == actualResult)
+  }
+}
